b2c3b9c60b0bacb935b1ce4a71c524c9087ddf8a
varun-tandon <vrntandon@gmail.com>
2016-09-13 21:52:44 -0700

    Update Level11.java

--- src/Level11.java
+++ src/Level11.java
@@ -1,9 +1,9 @@
 public class Level11 implements Level {
 	
-	public int[] KEY_1_LOC = new int[]{1 + (int)(Math.random() * ((2 - 1) + 1)), 0 + (int)(Math.random() * ((5 - 0) + 1))};
-	public int[] KEY_2_LOC = new int[]{1 + (int)(Math.random() * ((2 - 1) + 1)), 0 + (int)(Math.random() * ((5 - 0) + 1))};
-	public int[] DOOR_LOC = new int[]{3,2};
-	public int[] ENEMY_LOC = new int[]{4 + (int)(Math.random() * ((7 - 4) + 1)), 0 + (int)(Math.random() * ((5 - 0) + 1))};
+	public static int[] KEY_1_LOC = new int[]{1 + (int)(Math.random() * ((2 - 1) + 1)), 0 + (int)(Math.random() * ((5 - 0) + 1))};
+	public static int[] KEY_2_LOC = new int[]{1 + (int)(Math.random() * ((2 - 1) + 1)), 0 + (int)(Math.random() * ((5 - 0) + 1))};
+	public static int[] DOOR_LOC = new int[]{3,2};
+	public static int[] ENEMY_LOC = new int[]{4 + (int)(Math.random() * ((7 - 4) + 1)), 0 + (int)(Math.random() * ((5 - 0) + 1))};
 
 	@Override
 	public void load(World world, Player player) {
