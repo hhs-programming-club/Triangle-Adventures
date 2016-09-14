5ba70c5cea8af50c40a88c82c0e9fdcc3f49ae92
varun-tandon <vrntandon@gmail.com>
2016-09-13 21:41:02 -0700

    Update Level10.java

--- src/Level10.java
+++ src/Level10.java
@@ -1,8 +1,8 @@
 public class Level10 implements Level {
 	
-	public int[] KEY_LOC = new int[]{1 + (int)(Math.random() * ((2 - 1) + 1)), 0 + (int)(Math.random() * ((5 - 0) + 1))};
-	public int[] DOOR_LOC = new int[]{3,2};
-	public int[] ENEMY_LOC = new int[]{4 + (int)(Math.random() * ((7 - 4) + 1)), 0 + (int)(Math.random() * ((5 - 0) + 1))};
+	public static int[] KEY_LOC = new int[]{1 + (int)(Math.random() * ((2 - 1) + 1)), 0 + (int)(Math.random() * ((5 - 0) + 1))};
+	public static int[] DOOR_LOC = new int[]{3,2};
+	public static int[] ENEMY_LOC = new int[]{4 + (int)(Math.random() * ((7 - 4) + 1)), 0 + (int)(Math.random() * ((5 - 0) + 1))};
 
 	@Override
 	public void load(World world, Player player) {
